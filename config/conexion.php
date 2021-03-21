acade0b7926869e5f291a4c330a47c75dd94b021
Joseph Rios Henao <joseph232005@gmail.com>
2021-03-20 20:54:55 -0500

    Conexion BD

---
+++ config/conexion.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    class Connect {
+        protected $dbh;
+
+        protected function Conexion(){
+            try {
+                $connect = $this->dbh = new PDO("mysql:local=localhost;dbname=helpdesk","root","");
+                return $connect;
+            } catch (Exception $e) {
+                print "¡Error BD!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+
+        public function setNames(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+
+        public function route(){
+            return "http://localhost/HelpDesk/";
+        }
+    }
+?>
